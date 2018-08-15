551ff9615ee40324233ce5319af10257d159ce92
Paul Schoenfelder <paulschoenfelder@gmail.com>
2018-08-15 12:09:12 -0400

    Version 1.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HashRing.Mixfile do
   def project do
     [
       app: :libring,
-      version: "1.3.2",
+      version: "1.4.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
